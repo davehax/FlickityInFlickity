9148105df37f642370217e06e1a0faaba6abb2fd
davehax <dave.21.65@gmail.com>
2017-01-03 15:32:33 +1100

    Commit
    Proof of concept that a Flickity slider can be embedded in another
    Flickity slider

---
+++ test.js
@@ -0,0 +1,9 @@
+var flky = new Flickity('.carousel', {
+    draggable: false,
+    autoPlay: 8000,
+    wrapAround: true
+});
+var flky2 = new Flickity('.inner-carousel', {
+    draggable: false,
+    wrapAround: true
+});
